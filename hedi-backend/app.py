2bf56345986b3eefe8db6b8e2027479949f487cd
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2019-10-30 22:29:59 +0700

    Added /result/ endpoint

--- hedi-backend/app.py
+++ hedi-backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello from HeDi!"
+
+
+@app.route("/result/", methods=['POST'])
+def result():
+    payload = request.get_json()
+    return jsonify(payload)
 
 
 if __name__ == "__main__":
