00959eccfa50912d355f217cc9c161afafefe179
vince10 <adi.vanbasten99@gmail.com>
2019-11-04 15:42:40 +0700

    add entropy formula function

--- hedi-backend/decision_tree.py
+++ hedi-backend/decision_tree.py
@@ -1,1 +1,12 @@
 # Module for main logic of the app
+
+import math
+from os.path import dirname
+
+import numpy as np
+from sklearn.datasets.base import load_data
+
+
+def entropy_formula(class_number, group_number):
+    return -(class_number*1.0/group_number) * \
+           math.log2(class_number*1.0/group_number)
