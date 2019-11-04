8f8d88be46ed5ee5870e6e8c7d0baffa89f9a7f6
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2019-11-04 20:25:05 +0700

    Added method to load a dataset

--- hedi-backend/dataset.py
+++ hedi-backend/dataset.py
@@ -1,5 +1,7 @@
 import csv
-from os.path import join
+from os.path import join, dirname
+
+import numpy as np
 
 
 class DatasetContainer(dict):
@@ -18,3 +20,20 @@ class DatasetContainer(dict):
 
     def __dir__(self):
         return self.keys()
+
+
+def load_dataset(dataset_name: str):
+    with open(join(dirname(__file__), 'data', dataset_name)) as csv_file:
+        data_file = csv.reader(csv_file)
+        header_row = next(data_file)
+        number_samples = int(header_row[0])
+        number_features = int(header_row[1])
+        target_attribute_names = np.array(header_row[2:])
+        data_attribute = np.empty(shape=(number_samples, number_features))
+        target_attribute = np.empty(shape=(number_samples,), dtype=np.int)
+
+        for index, sample in enumerate(data_file):
+            data_attribute[index] = np.asarray(sample[:-1], dtype=np.float64)
+            target_attribute[index] = np.asarray(sample[-1], dtype=np.int)
+
+    return data_attribute, target_attribute, target_attribute_names
