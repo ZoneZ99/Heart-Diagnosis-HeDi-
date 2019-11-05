94a2475bd22e797a39d441e1822edbe4eb236bc0
vince10 <adi.vanbasten99@gmail.com>
2019-11-05 12:18:11 +0700

    revised entropy_calculation

--- hedi-backend/decision_tree.py
+++ hedi-backend/decision_tree.py
@@ -8,15 +8,12 @@ from sklearn.datasets.base import load_data
 
 
 def entropy_formula(class_number, group_number):
-    return -(class_number*1.0/group_number) * \
-           math.log2(class_number*1.0/group_number)
+    return -(class_number*1.0/group_number) \ math.log2(class_number*1.0/group_number)
 
-def entropy_cal(c1, c2):
-    """
-    Returns entropy of a group of data
-    c1: count of one class
-    c2: count of another class
-    """
-    if c1== 0 or c2 == 0:  # when there is only one class in the group, entropy is 0
-        return 0
-    return entropy_func(c1, c1+c2) + entropy_func(c2, c1+c2)
+
+def entropy_calculation(class1,class2):
+    group_number = class1 +class2
+    if class1 > 0  and class2 >0:
+        return entropy_formula(class1, group_number)+entropy_formula(class2,group_number)
+    return 0
+
