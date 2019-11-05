9f0d03b142d8778adc2c9993762e987034109987
vince10 <adi.vanbasten99@gmail.com>
2019-11-05 12:37:13 +0700

    add entropy division function

--- hedi-backend/decision_tree.py
+++ hedi-backend/decision_tree.py
@@ -8,7 +8,7 @@ from sklearn.datasets.base import load_data
 
 
 def entropy_formula(class_number, group_number):
-    return -(class_number*1.0/group_number) \ math.log2(class_number*1.0/group_number)
+    return -(class_number*1.0/group_number) * math.log2(class_number*1.0/group_number)
 
 
 def entropy_calculation(class1,class2):
@@ -16,4 +16,13 @@ def entropy_calculation(class1,class2):
     if class1 > 0  and class2 >0:
         return entropy_formula(class1, group_number)+entropy_formula(class2,group_number)
     return 0
+
+def entropy_one_division(division):
+    sum_entropy = 0
+    num_of_division = len(division)
+    classess = set(division)
+
+    for clas in classes:
+        sum_entropy += sum(division == clas)*1.0/num_of_divission * entropy_calculation(sum(division == clas ),sum(division != clas))
+    return sum_entropy, num_of_division
 
