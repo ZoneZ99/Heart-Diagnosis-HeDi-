9d43f7bd1c1faa98f8e52f6865b1320d49543235
alfianfuadi17 <alfianfuadi17@gmail.com>
2019-11-07 08:21:35 +0700

    Added initial DecisionTreeClassifier

--- hedi-backend/decision_tree.py
+++ hedi-backend/decision_tree.py
@@ -1,1 +1,20 @@
-# Module for main logic of the app
+import numpy as np
+
+class DecisionTreeClassifier:
+
+    def __init__(self, dataset, max_depth):
+        self.dataset = dataset
+        self.depth = 0
+        self.max_depth = max_depth
+
+    def find_best_split(self, split_column, target_variable):
+        min_entropy = 10
+        n = len(target_variable)
+
+        for value in set(split_column):
+            y_predict = split_column < value
+            entropy = get_entropy(y_predict, target_variable)
+            if entropy <= min_entropy:
+                min_entropy = entropy
+                cutoff = value
+        return min_entropy, cutoff
