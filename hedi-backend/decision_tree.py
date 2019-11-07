87c38e72fecd74953aa74177810aa4dbdfaf7c3f
alfianfuadi17 <alfianfuadi17@gmail.com>
2019-11-07 08:49:40 +0700

    Added splitting then fitting

--- hedi-backend/decision_tree.py
+++ hedi-backend/decision_tree.py
@@ -23,7 +23,7 @@ class DecisionTreeClassifier:
 		column = None
 		min_entropy = 1
 		cutoff = None
-		
+
 		for i, c in enumerate(x.T):
 			entropy, current_cutoff = self.find_best_split(c, y)
 			if not entropy:
@@ -33,3 +33,30 @@ class DecisionTreeClassifier:
 				column = i
 				cutoff = current_cutoff
 		return column, cutoff, min_entropy
+
+	def all_same(self, items):
+		return all(x == items[0] for x in items)
+
+	def fit(self, feature_set, target_variable, parent_node={}, depth=0):
+		if parent_node is None:
+			return None
+		elif len(target_variable) == 0:
+			return None
+		elif self.all_same(target_variable):
+			return {'val': target_variable[0]}
+		elif depth >= self.max_depth:
+			return None
+		else:
+			column, cutoff, entropy = self.find_best_split_of_all(feature_set, target_variable)
+			y_left = target_variable[feature_set[:, column] < cutoff]
+			y_right = target_variable[feature_set[:, column] >= cutoff]
+			parent_node = {'col': self.dataset.feature_names[column],
+						   'index_col': column,
+						   'cutoff': cutoff,
+						   'val': np.round(np.mean(target_variable)),
+						   'left': self.fit(feature_set[feature_set[:, column] < cutoff], y_left, {}, depth+1),
+						   'right': self.fit(feature_set[feature_set[:, column] >= cutoff], y_right, {}, depth+1)
+						   }
+			self.depth += 1
+			self.trees = parent_node
+			return parent_node
