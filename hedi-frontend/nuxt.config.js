c9d0b0cd68230a53e1ca20829f4afe73ba090607
ahmad364 <ahmadreyz@gmail.com>
2019-11-07 15:24:51 +0700

    form finished

--- hedi-frontend/nuxt.config.js
+++ hedi-frontend/nuxt.config.js
@@ -12,7 +12,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Signika:400,700&display=swap' }
     ]
   },
   /*
@@ -54,7 +55,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
