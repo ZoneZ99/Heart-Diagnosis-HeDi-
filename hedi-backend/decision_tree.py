055324ecae9139449ff0cad02f1490fed9781c18
ahmad364 <ahmadsupri1999@gmail.com>
2019-11-19 16:19:48 +0700

    Add helper method to fit and prune:

--- hedi-backend/decision_tree.py
+++ hedi-backend/decision_tree.py
@@ -37,7 +37,6 @@ class DecisionTreeClassifier:
 
     def __init__(self, dataset, max_depth):
         self.dataset = dataset
-        self.depth = 0
         self.max_depth = max_depth
 
     def find_best_split(self, split_column, target_variable):
@@ -68,20 +67,20 @@ class DecisionTreeClassifier:
     def all_same(self, items):
         return all(x == items[0] for x in items)
 
+    def fit_and_prune(self, feature_set, target_variable, parent_node={}, depth=0):
+        self.fit(self, feature_set, target_variable, parent_node, depth)
+        self.prune(self.trees)
+
     def fit(self, feature_set, target_variable, parent_node={}, depth=0):
         if parent_node is None:
             return None
         elif len(target_variable) == 0:
             return None
         elif self.all_same(target_variable):
-<<<<<<< HEAD
-            return {'val': target_variable[0].item()}
-=======
             return {'val': target_variable[0],
                     'Positive(1)': list(target_variable).count(1),
                     'Negative(0)': list(target_variable).count(0),
                     'IsLeafNode': True}
->>>>>>> 8465a7419f410a8909f69a7e5c24a2ccfcb6b7c8
         elif depth >= self.max_depth:
             return {'Positive(1)': list(target_variable).count(1),
                     'Negative(0)': list(target_variable).count(0),
@@ -91,18 +90,6 @@ class DecisionTreeClassifier:
             column, cutoff, entropy = self.find_best_split_of_all(feature_set, target_variable)
             y_left = target_variable[feature_set[:, column] < cutoff]
             y_right = target_variable[feature_set[:, column] >= cutoff]
-<<<<<<< HEAD
-            parent_node = {
-                'col': self.dataset.feature_names[column],
-                'index_col': column,
-                'cutoff': cutoff.item(),
-                'Positive(1)': list(target_variable).count(1),
-                'Negative(0)': list(target_variable).count(0),
-                'val': np.round(np.mean(target_variable)).item(),
-                'left': self.fit(feature_set[feature_set[:, column] < cutoff], y_left, {}, depth + 1),
-                'right': self.fit(feature_set[feature_set[:, column] >= cutoff], y_right, {}, depth + 1)
-            }
-=======
             parent_node = {'col': self.dataset.feature_names[column],
                            'index_col': column,
                            'cutoff': cutoff,
@@ -113,8 +100,6 @@ class DecisionTreeClassifier:
                            'left': self.fit(feature_set[feature_set[:, column] < cutoff], y_left, {}, depth+1),
                            'right': self.fit(feature_set[feature_set[:, column] >= cutoff], y_right, {}, depth+1)
                            }
->>>>>>> 8465a7419f410a8909f69a7e5c24a2ccfcb6b7c8
-            self.depth += 1
             self.trees = parent_node
             return parent_node
 
@@ -138,54 +123,54 @@ class DecisionTreeClassifier:
 
 
 
-def can_be_added_to_queue(tree):
-    has_children = tree.get('left') is not None or tree.get('right') is not None
-    has_sub_trees = not tree.get('left')['IsLeafNode'] and not tree.get('right')['IsLeafNode']
-    return tree is not None and has_children and has_sub_trees
+    def can_be_added_to_queue(tree):
+        has_children = tree.get('left') is not None or tree.get('right') is not None
+        has_sub_trees = not tree.get('left')['IsLeafNode'] and not tree.get('right')['IsLeafNode']
+        return tree is not None and has_children and has_sub_trees
 
 
-def prune(tree):
-    priority_queue = [tree]
+    def prune(self, tree):
+        priority_queue = [tree]
 
-    while len(priority_queue) > 0:
+        while len(priority_queue) > 0:
 
-        current_node = priority_queue[0]
-        chisquare_current = count_chisquare(current_node)
-        chisquare_left = count_chisquare(current_node['left'])
-        chisquare_right = count_chisquare(current_node['right'])
-        if (chisquare_left > chisquare_current) or (chisquare_right > chisquare_current):
-            if current_node['left'] in priority_queue:
-                priority_queue.remove(current_node['left'])
-            if current_node['right'] in priority_queue:
-                priority_queue.remove(current_node['right'])
-            current_node['left'] = None
-            current_node['right'] = None
-            current_node['IsLeafNode'] = True
-        else:
-            if can_be_added_to_queue(current_node['left']):
-                priority_queue.append(current_node['left'])
-            if can_be_added_to_queue(current_node['right']):
-                priority_queue.append(current_node['right'])
-        priority_queue.remove(priority_queue[0])
-
-
-def count_chisquare(tree):
-    num_of_parent_population = tree['Positive(1)'] + tree['Negative(0)']
-    chance_positive = tree['Positive(1)'] / num_of_parent_population
-    chance_negative = tree['Negative(0)'] / num_of_parent_population
-    left_child = tree['left']
-    right_child = tree['right']
-
-    chisquare_left_positive = count_chisquare_util(left_child['Positive(1)'] + left_child['Negative(0)'],
-                                                   chance_positive, left_child['Positive(1)'])
-    chisquare_left_negative = count_chisquare_util(left_child['Positive(1)'] + left_child['Negative(0)'],
-                                                   chance_negative, left_child['Negative(0)'])
-    chisquare_right_positive = count_chisquare_util(right_child['Positive(1)'] + right_child['Negative(0)'],
-                                                    chance_positive, right_child['Positive(1)'])
-    chisquare_right_negative = count_chisquare_util(right_child['Positive(1)'] + right_child['Negative(0)'],
-                                                    chance_negative, right_child['Negative(0)'])
-    return chisquare_left_negative + chisquare_left_positive + chisquare_right_negative + chisquare_right_positive
+            current_node = priority_queue[0]
+            chisquare_current = self.count_chisquare(current_node)
+            chisquare_left = self.count_chisquare(current_node['left'])
+            chisquare_right = self.count_chisquare(current_node['right'])
+            if (chisquare_left > chisquare_current) or (chisquare_right > chisquare_current):
+                if current_node['left'] in priority_queue:
+                    priority_queue.remove(current_node['left'])
+                if current_node['right'] in priority_queue:
+                    priority_queue.remove(current_node['right'])
+                current_node['left'] = None
+                current_node['right'] = None
+                current_node['IsLeafNode'] = True
+            else:
+                if self.can_be_added_to_queue(current_node['left']):
+                    priority_queue.append(current_node['left'])
+                if self.can_be_added_to_queue(current_node['right']):
+                    priority_queue.append(current_node['right'])
+            priority_queue.remove(priority_queue[0])
 
 
-def count_chisquare_util(total_population, chance, class_population):
-    return (total_population * chance - class_population) ** 2 / (total_population * chance)
+    def count_chisquare(self, tree):
+        num_of_parent_population = tree['Positive(1)'] + tree['Negative(0)']
+        chance_positive = tree['Positive(1)'] / num_of_parent_population
+        chance_negative = tree['Negative(0)'] / num_of_parent_population
+        left_child = tree['left']
+        right_child = tree['right']
+
+        chisquare_left_positive = self.count_chisquare_util(left_child['Positive(1)'] + left_child['Negative(0)'],
+                                                    chance_positive, left_child['Positive(1)'])
+        chisquare_left_negative = self.count_chisquare_util(left_child['Positive(1)'] + left_child['Negative(0)'],
+                                                    chance_negative, left_child['Negative(0)'])
+        chisquare_right_positive = self.count_chisquare_util(right_child['Positive(1)'] + right_child['Negative(0)'],
+                                                        chance_positive, right_child['Positive(1)'])
+        chisquare_right_negative = self.count_chisquare_util(right_child['Positive(1)'] + right_child['Negative(0)'],
+                                                        chance_negative, right_child['Negative(0)'])
+        return chisquare_left_negative + chisquare_left_positive + chisquare_right_negative + chisquare_right_positive
+
+
+    def count_chisquare_util(self, total_population, chance, class_population):
+        return (total_population * chance - class_population) ** 2 / (total_population * chance)
